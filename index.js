4df93228ed83d95e27d010f75d36c034b9f3ee4e
M Roshaan Jamshaid <71185942+MRoshaanJamshaid@users.noreply.github.com>
2020-10-05 11:51:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var playerOne=prompt("Enter Player 1 Name: ");
+var playerSecond=prompt("Enter Player 2 Name: ");
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".PNG"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".PNG";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩  "+playerOne.toUpperCase()+" Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩  "+playerSecond.toUpperCase()+" Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
