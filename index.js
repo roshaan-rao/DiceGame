d3983a5cfec76600aee646855935c74efa50e48e
M Roshaan Jamshaid <71185942+MRoshaanJamshaid@users.noreply.github.com>
2020-10-05 11:55:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+var playerOne=prompt("Enter Player 1 Name: ");
+var playerSecond=prompt("Enter Player 2 Name: ");
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
@@ -20,10 +21,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩  Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩  "+playerOne.toUpperCase()+" Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "🚩  Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "🚩  "+playerSecond.toUpperCase()+" Wins!";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
